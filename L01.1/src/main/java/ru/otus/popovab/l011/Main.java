1046bd5952373b7f3c355be2eafc9c50781a1b5c
Vist73rus <popov73rus@gmail.com>
2019-02-17 21:40:31 +0300

    L01.1

---
+++ L01.1/src/main/java/ru/otus/popovab/l011/Main.java
@@ -0,0 +1,46 @@
+package ru.otus.popovab.l011;
+
+import java.util.*;
+import java.util.concurrent.Callable;
+
+import com.google.common.collect.Lists;
+
+public class Main {
+    private static final int MEASURE_COUNT = 1;
+
+    public static void main(String... args) throws Exception {
+        Collection<Integer> example = new ArrayList<>();
+        int min = 0;
+        int max = 999;
+        for (int i = min; i < max + 1; i++) {
+            example.add(i);
+        }
+
+        Collections.shuffle((List<Integer>)example);
+
+        List<Integer> resultCall = new ArrayList<>();
+        calcTimeCall(() -> resultCall.addAll(Lists.reverse((List<Integer>)example)));
+
+        List<Integer> resultRun = new ArrayList<>();
+        calcTimeRun(() -> resultRun.addAll(Lists.reverse((List<Integer>)example)));
+    }
+
+    private static void calcTimeRun(Runnable runnable) {
+        long startTime = System.nanoTime();
+        for (int i = 0; i < MEASURE_COUNT; i++)
+            runnable.run();
+        long finishTime = System.nanoTime();
+        long timeNs = (finishTime - startTime) / MEASURE_COUNT;
+        System.out.println("Run time spent: " + timeNs + "ns (" + timeNs / 1_000_000 + "ms)");
+    }
+
+    private static void calcTimeCall(Callable callable) throws Exception {
+        long startTime = System.nanoTime();
+        for (int i = 0; i < MEASURE_COUNT; i++) {
+            callable.call();
+        }
+        long finishTime = System.nanoTime();
+        long timeNs = (finishTime - startTime) / MEASURE_COUNT;
+        System.out.println("Call time spent: " + timeNs + "ns (" + timeNs / 1_000_000 + "ms)");
+    }
+}
